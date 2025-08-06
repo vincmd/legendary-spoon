b53b9fac204ccf51d79ca01b420b24850a6d4a14
RPQ35 <alfajriropiq@gmail.com>
2025-08-06 10:39:08 +0800

    template final

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -1,17 +1,18 @@
 @include('template.body')
 <main class="h-full overflow-y-auto">
     <div class="container px-6 mx-auto grid">
+        {{-- title --}}
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
             Dashboard
         </h2>
 
-        @include('template.parts.cta')
+        {{-- @include('template.parts.cta') --}}
 
-        @include('template.parts.card')
+        {{-- @include('template.parts.card') --}}
 
-        @include('template.parts.table.table')
+        {{-- @include('template.parts.table.table') --}}
 
-        @include('template.parts.chart.chart-parrent')
+        {{-- @include('template.parts.chart.chart-parrent') --}}
     </div>
 </main>
 
---
+++ resources/views/template/sidebar/sidebar-mobile.blade.php
@@ -0,0 +1,51 @@
+<!-- Mobile sidebar -->
+<!-- Backdrop -->
+<div x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150"
+    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+    class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+<aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
+    x-show="isSideMenuOpen" x-transition:enter="transition ease-in-out duration-150"
+    x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100"
+    x-transition:leave-end="opacity-0 transform -translate-x-20" @click.away="closeSideMenu"
+    @keydown.escape="closeSideMenu">
+    <div class="py-4 text-gray-500 dark:text-gray-400">
+        <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+            Windmill
+        </a>
+        <ul class="mt-6">
+            @foreach ($sidebar_buttons as $side_button)
+            @if ($side_button['status'])
+            <ul >
+                <li class="relative px-6 py-3">
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                    aria-hidden="true"></span>
+                    <a class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                    href="{{ $side_button['href'] }}">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    {!! $side_button['icon'] !!}
+                </svg>
+                            <span class="ml-4">{{ $side_button['name'] }}</span>
+                        </a>
+                    </li>
+                </ul>
+                @else
+                <li class="relative px-6 py-3">
+                    <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ $side_button['href'] }}">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                    stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                    {!! $side_button['icon'] !!}
+                </svg>
+                <span class="ml-4">{{ $side_button['name'] }}</span>
+            </a>
+                </li>
+                @endif
+
+                @endforeach
+            </ul>
+
+        </div>
+    </aside>
