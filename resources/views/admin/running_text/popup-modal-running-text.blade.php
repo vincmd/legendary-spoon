4752970455af1214c5fd5d1e4962579647274d76
RPQ35 <alfajriropiq@gmail.com>
2025-08-08 10:19:13 +0800

    runing fix

--- resources/views/admin/running_text/popup-modal-running-text.blade.php
+++ resources/views/admin/running_text/popup-modal-running-text.blade.php
@@ -1,7 +1,10 @@
 @include('template.modal.modal-part.modal-parent')
 <form action="{{ route('update_text') }}" method="post">
     @csrf
-    @include('template.modal.modal-part.modal-body')
+    {{-- isi form --}}
+    @include('admin.running_text.modal-runnign-text-body')
+
+    {{-- button (close , submit) --}}
     @include('template.modal.modal-part.footer-button')
 </form>
 
--- resources/views/template/modal/modal-part/modal-body.blade.php
+++ resources/views/template/modal/modal-part/modal-body.blade.php
@@ -8,6 +8,6 @@
     <label class="block text-sm">
         <textarea name="text" type="text"
             class=" mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input w-full h-full  text-start flex flex-col items-left justify-start max-w-full break-words p-0"
-            placeholder="plat" >{{ $running_text }}</textarea>
+            placeholder="plat" ></textarea>
     </label>
 </div>
