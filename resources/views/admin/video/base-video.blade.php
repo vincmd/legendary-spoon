3e52267bace30f92fa60ac2e18395f05a35b0e1f
Vincent <23_vincenttheodores@student.smkti.net>
2025-08-08 13:21:28 +0800

    tugas

--- resources/views/admin/video/base-video.blade.php
+++ resources/views/admin/video/base-video.blade.php
@@ -3,9 +3,13 @@
 <div
 class="w-9/10 px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800"
 >
+@if($video)@if($video->file_path !==null)
 <video  src="{{ asset('storage/'.$video->file_path) }}"  controls>
 
 </video>
+@endif
+
+@endif
 
 
 </div>
