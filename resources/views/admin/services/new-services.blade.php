47ae880f562c96148555a3ff952514377a38176b
RPQ35 <alfajriropiq@gmail.com>
2025-08-11 15:35:51 +0800

    small update

--- resources/views/admin/services/new-services.blade.php
+++ resources/views/admin/services/new-services.blade.php
@@ -11,7 +11,7 @@
                 <form enctype="multipart/form-data" action="{{ route('services.create') }}" method="post">
                     @csrf
 
-                    <x-clasoc-input-component name="service_name" label="service name"
+                    <x-clasoc-input-component name="services_name" label="service name"
                         placeholder="ganti plat"></x-clasoc-input-component>
 
                     <x-clasoc-input-component name="code" label="code" placeholder="A"></x-clasoc-input-component>
---
+++ resources/views/template/second-body.blade.php
@@ -0,0 +1,18 @@
+
+@include('template.html-head')
+
+<body>
+    {{-- @include('template.header') --}}
+    <div
+    class="flex h-screen bg-gray-50 dark:bg-gray-900"
+    :class="{ 'overflow-hidden': isSideMenuOpen }"
+    >
+    {{-- @include('template.sidebar-name') --}}
+
+      <div class="flex flex-col flex-1 w-full">
+
+@yield('main')
+
+
+@include('template.close')
+
